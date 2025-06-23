81a5a5de72043ad756f51568267773b9bf135d59
Raikwar <gourav.raikwar@capgemini.com>
2025-06-23 19:08:59 +0530

    Initial commit

---
+++ CorsGlobalConfig.java
@@ -0,0 +1,26 @@
+package com.carwash.api_gateway.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsGlobalConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:5173"); // your frontend
+        config.addAllowedMethod("*"); // GET, POST, PUT, DELETE etc.
+        config.addAllowedHeader("*"); // Accept all headers
+        config.setAllowCredentials(true); // Send credentials like cookies or Authorization
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
